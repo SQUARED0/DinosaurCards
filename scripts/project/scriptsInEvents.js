e948d8cd1eae96ee6cb4b8ff1c8d09f47e1028c8
SQUARED <141570367+SQUARED0@users.noreply.github.com>
2024-04-21 14:41:20 +0300

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,21 @@
+
+
+
+const scriptsInEvents = {
+
+	async Emenu_Event18_Act1(runtime, localVars)
+	{
+		
+		document.addEventListener('keydown', function(event) {
+		
+		  if (event.key === 'F11') {
+		    event.preventDefault(); 
+		    
+		  }
+		});
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
